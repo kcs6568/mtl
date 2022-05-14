f963ab1612793269e99584440897e6e9e39084f0
kcs6568 <kcs6568@gmail.com>
2022-05-14 17:08:40 +0000

    add module

---
+++ generate_weights.py
@@ -0,0 +1,27 @@
+from collections import OrderedDict
+
+import os
+import torch
+import torch.nn as nn
+import glob
+
+import torchvision
+from torchvision.models.resnet import resnet50
+
+params = torch.load('/root/.cache/torch/hub/checkpoints/resnet50-0676ba61.pth')
+
+new_layer = OrderedDict()
+new_stem = OrderedDict() 
+for k, v in params.items():
+    if 'layer' in k:
+        new_layer[k] = v
+        
+    else:
+        if not 'fc' in k:
+            k_ = k.replace('1', '')
+            new_stem[k_] = v        
+
+        
+torch.save(new_layer, '/root/volume/pre_weights/resnet50_IM1K_only_layer.pth')
+torch.save(new_stem, '/root/volume/pre_weights/resnet50_IM1K_only_stem.pth')
+            
